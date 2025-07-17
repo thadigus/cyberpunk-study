ea459f878a25355f6024c1437315312e30847508
thadigus <turner.w.thad@gmail.com>
2025-07-16 22:27:08 -0400

    Better padding on icons for mobile

--- src/DarkSwitcher.jsx
+++ src/DarkSwitcher.jsx
@@ -16,7 +16,7 @@ function ToggleTheme() {
         setTheme(theme === 'light' ? 'dark':'light');
     }
   return (
-    <button onClick={toggleTheme} className=''> {theme === 'light' ? <FontAwesomeIcon icon={faMoon} className="text-text-light" /> : <FontAwesomeIcon icon={faSun} className="text-yellow-400" />}</button>
+    <button onClick={toggleTheme} className=''> {theme === 'light' ? <FontAwesomeIcon icon={faMoon} className="text-text-light p-4 lg:px-6" /> : <FontAwesomeIcon icon={faSun} className="text-yellow-400 p-4 lg:px-6" />}</button>
   )
 }
 
--- src/Header.jsx
+++ src/Header.jsx
@@ -10,7 +10,7 @@ const Header = () => (
         <span className="self-center text-xl font-semibold whitespace-nowrap p-4">Cyberpunk Study</span>
       </a>
       <div className="flex items-center lg:order-2">
-        <a href="https://thadturner.com" className="font-semibold p-4"> <FontAwesomeIcon icon={faUser} /> </a>
+        <a href="https://thadturner.com" className="font-semibold p-4 lg:px-6"> <FontAwesomeIcon icon={faUser} /> </a>
         <ToggleTheme />
       </div>
     </div>
